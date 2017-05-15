eb0f3493fa88f3120ff4a16151fd9513ffab6a48
Larry Hignight <larry.hignight@gmail.com>
2017-05-14 21:23:49 -0700

    Updated the grade data;  Created a new table in grades.Rmd;  Added scripts to rotate images on the Pi

---
+++ Wyatt-Grades/4-take-screenshots.sh
@@ -0,0 +1,2 @@
+# Use scrot -s -d 8 filename 
+# to take screenshots of the html files
---
+++ Wyatt-Grades/5-cvlc-script.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install vlc
+cvlc grades.jpg last-assignments.jpg last-assignments-2.jpg
